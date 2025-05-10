ed4ca6ddfbc99a767c031786a41f0eb33cb210f2
Jagan_tirumalai <jagan.tirumalai@gmail.com>
2025-05-10 19:02:58 -0400

    Updated JSON_generate.sql

--- JSON_generate.sql
+++ JSON_generate.sql
@@ -17,3 +17,25 @@ select json_object(key 'emps' value (
          )
         ) from emp e 
     )) from dual;    
+
+--Generate simple json document for all records
+select json_object(*) from emp;
+
+--Generate simple json document for all records pretty
+select json_serialize(json_object(* returning clob) pretty) from emp;
+
+--Generate json document for group of records
+select deptno,json_arrayagg(json_object(*)) from emp
+group by deptno;
+
+--generate nested JSON Doc example
+select json_serialize(
+    json_array(json_object(key 'DEPT' value
+                    json_array(json_object(key 'DEPTNAME' value d.dname,
+                                            key 'EMPS' value e.emp_json)))) 
+            pretty) dept_emp_json
+from 
+dept d,
+(select deptno,json_arrayagg(json_object(*)) emp_json from emp
+group by deptno) e
+where d.deptno=e.deptno
